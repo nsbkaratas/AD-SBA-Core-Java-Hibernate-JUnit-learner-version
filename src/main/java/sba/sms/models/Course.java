08b425494c38fd53fd7f495830c2cafa50c62bc2
Nesibe Karatas <nsbkaratas@gmail.com>
2023-01-26 14:25:58 -0500

    models completed

--- src/main/java/sba/sms/models/Course.java
+++ src/main/java/sba/sms/models/Course.java
@@ -1,5 +1,70 @@
 package sba.sms.models;
 
-public class Course {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Setter
+@Getter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+@Entity
+@Table(name="course")
+public class Course {
+	    @Id 
+	    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+	    @Column(name = "id")
+	    int id;
+	    @NonNull
+	    @Column(length = 50, name = "name")
+	    String name;
+	    @NonNull
+	    @Column(length = 50, name="instructor")
+	    String instructor;
+	    
+	    @ToString.Exclude
+	    @ManyToMany(mappedBy = "courses", cascade= {CascadeType.PERSIST, CascadeType.MERGE,CascadeType.REFRESH,CascadeType.DETACH},fetch = FetchType.EAGER)
+	    List<Student> student= new ArrayList<>();
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(id, instructor, name);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Course other = (Course) obj;
+			return id == other.id && Objects.equals(instructor, other.instructor) && Objects.equals(name, other.name);
+		}
+	    
 }
--- src/main/java/sba/sms/models/Student.java
+++ src/main/java/sba/sms/models/Student.java
@@ -1,5 +1,73 @@
 package sba.sms.models;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+
+@Entity
+@Table(name = "student")
+
+public class Student {
+	@NonNull
+    @Id 
+    @Column(length = 50,name = "email", unique = true)
+    String email;
+    @NonNull
+    @Column(length = 50, nullable = false, name = "name")
+    String name;
+    @NonNull
+    @Column(length = 50,name = "password")
+    String password;
+    
+    @ToString.Exclude
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.REFRESH, CascadeType.DETACH}, fetch = FetchType.EAGER)
+    @JoinTable(name = "student_courses", joinColumns = @JoinColumn(name="student_email"), inverseJoinColumns = @JoinColumn(name="course_id"))
+    List<Course> course = new ArrayList<>();
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, name, password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Student other = (Student) obj;
+		return Objects.equals(email, other.email) && Objects.equals(name, other.name)
+				&& Objects.equals(password, other.password);
+	}
+    
+    
 }
