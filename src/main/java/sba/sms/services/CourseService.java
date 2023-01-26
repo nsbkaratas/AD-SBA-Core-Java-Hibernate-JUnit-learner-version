0e92e567f41cc45dc7be00fa47577d802a7397f6
Nesibe Karatas <nsbkaratas@gmail.com>
2023-01-26 14:28:10 -0500

    interface implementation started

--- src/main/java/sba/sms/services/CourseService.java
+++ src/main/java/sba/sms/services/CourseService.java
@@ -1,8 +1,73 @@
 package sba.sms.services;
 
+import java.util.ArrayList;
+import java.util.List;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
 
+import sba.sms.dao.CourseI;
+import sba.sms.models.Course;
+import sba.sms.utils.HibernateUtil;
 
-public class CourseService  {
+public class CourseService implements CourseI  {
+
+	@Override
+	public void createCourse(Course course) {
+		// TODO Auto-generated method stub
+		Transaction tx = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			tx=session.beginTransaction();
+			session.merge(course);
+			tx.commit();
+			
+		}catch (HibernateException ex) {
+			ex.printStackTrace();
+			tx.rollback();
+		}finally {
+			session.close();
+		}
+		
+		
+	}
+
+	@Override
+	public Course getCourseById(int courseId) {
+		Transaction tx= null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		Course course = new Course();
+		
+		try {
+			tx= session.beginTransaction();
+			Query<Course> query = session.createQuery("from Course where id =:id", Course.class)
+					.setParameter("id", courseId);
+			course = query.getSingleResult();
+			tx.commit();
+			
+		} catch (HibernateException ex) {
+			ex.printStackTrace();
+			tx.rollback();
+		}finally {
+			session.close();
+		}
+		return course;
+	}
+
+	@Override
+	public List<Course> getAllCourses() {
+		List<Course> listOfCourses = new ArrayList<>();
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			Query<Course> query = session.createQuery("from Course ", Course.class);
+			listOfCourses = query.getResultList();
+		} catch (HibernateException ex) {
+			ex.printStackTrace();
+		}
+		
+		return listOfCourses;
+	}
 
 }
--- src/main/java/sba/sms/services/StudentService.java
+++ src/main/java/sba/sms/services/StudentService.java
@@ -1,5 +1,90 @@
 package sba.sms.services;
 
-public class StudentService  {
+import java.util.ArrayList;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import sba.sms.dao.StudentI;
+import sba.sms.models.Course;
+import sba.sms.models.Student;
+import sba.sms.utils.HibernateUtil;
+
+public class StudentService implements StudentI {
+
+	@Override
+	public List<Student> getAllStudents() {
+		List<Student> listOfStudents = new ArrayList<>();
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			Query<Student> query = session.createQuery("from Student ", Student.class);
+			listOfStudents = query.getResultList();
+		} catch (HibernateException ex) {
+			ex.printStackTrace();
+		}
+		
+		return listOfStudents;
+	}
+
+	@Override
+	public void createStudent(Student student) {
+		Transaction tx = null;
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			tx=session.beginTransaction();
+			session.merge(student);
+			tx.commit();
+			
+		}catch (HibernateException ex) {
+			ex.printStackTrace();
+			tx.rollback();
+		}finally {
+			session.close();
+		}		
+		
+	}
+
+	@Override
+	public Student getStudentByEmail(String email) {
+		Transaction tx = null;
+		Student student = new Student();
+		Session session = HibernateUtil.getSessionFactory().openSession();
+		try {
+			tx = session.beginTransaction();
+			Query<Student> query = session.createQuery("from student where email = :email", Student.class)
+					.setParameter("email", email);
+			student = query.getSingleResult();
+			tx.commit();
+			
+			
+		} catch (HibernateException ex) {
+			ex.printStackTrace();
+			tx.rollback();
+		}finally {
+			session.close();
+		}
+		return student;
+	}
+
+	@Override
+	public boolean validateStudent(String email, String password) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void registerStudentToCourse(String email, int courseId) {
+		
+		
+	}
+
+	@Override
+	public List<Course> getStudentCourses(String email) {
+		
+		return null;
+	}
 
 }
